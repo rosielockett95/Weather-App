2b00e787b1923b95c174dafdf11dc8059a4c5e9b
rosielockett95 <rosie.lockett4@gmail.com>
2026-08-13 16:41:53 +0100

    metric/imperial unit changes on button pressed started

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Get users current geo location
+let lan;
+let lon;
+
 navigator.geolocation.getCurrentPosition(
   (position) => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
 
     getCurrentWeather(lat, lon);
   },
@@ -19,28 +22,48 @@ async function getCurrentWeather(lat, lon) {
 
   const data = await response.json();
 
-  console.log(data);
+  const imperialResponse = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&current=temperature_2m,precipitation,wind_speed_10m,apparent_temperature&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`,
+  );
 
-  document.querySelector("#currentTemp").textContent =
-    data.current.temperature_2m + "°C";
+  const imperialData = await imperialResponse.json();
 
-  document.querySelector("#feelsLikeTemp").textContent =
-    data.current.apparent_temperature + "°C";
+  document.querySelector("#currentTemp").textContent = document
+    .querySelector("#currentTemp")
+    .classList.contains("metric")
+    ? data.current.temperature_2m + "°C"
+    : imperialData.current.temperature_2m + "°F";
+
+  document.querySelector("#feelsLikeTemp").textContent = document
+    .querySelector("#feelsLikeTemp")
+    .classList.contains("metric")
+    ? data.current.apparent_temperature + "°C"
+    : imperialData.current.apparent_temperature + "°F";
 
   document.querySelector("#currentHumidity").textContent =
     data.current.relative_humidity_2m + "%";
 
   let currentWindSpeed = Math.round(data.current.wind_speed_10m);
+  let ImperialWindSpeed = Math.round(imperialData.current.wind_speed_10m);
 
-  document.querySelector("#currentWindSpeed").textContent =
-    currentWindSpeed + " " + "mph";
+  document.querySelector("#currentWindSpeed").textContent = document
+    .querySelector("#currentWindSpeed")
+    .classList.contains("metric")
+    ? currentWindSpeed + " " + "kmh"
+    : ImperialWindSpeed + " " + "mph";
 
-  document.querySelector("#currentPrecipitation").textContent =
-    data.current.precipitation + " " + "in";
+  document.querySelector("#currentPrecipitation").textContent = document
+    .querySelector("#currentPrecipitation")
+    .classList.contains("metric")
+    ? data.current.precipitation + " " + "mm"
+    : imperialData.current.precipitation + " " + "inch";
 
   const days = document.querySelectorAll(".days");
   const dailyForecastArrayMin = data.daily.temperature_2m_min;
   const dailyForecastArrayMax = data.daily.temperature_2m_max;
+
+  const imperialForecastArrayMin = imperialData.daily.temperature_2m_min;
+  const imperialForecastArrayMax = imperialData.daily.temperature_2m_max;
 
   days.forEach((day, index) => {
     const dates = new Date(data.daily.time[index]);
@@ -48,22 +71,32 @@ async function getCurrentWeather(lat, lon) {
     day.querySelector(".date").textContent = dates.toLocaleDateString("en-GB", {
       weekday: "long",
     });
-    day.querySelector(".min").textContent = `${dailyForecastArrayMin[index]}`;
-    day.querySelector(".max").textContent = `${dailyForecastArrayMax[index]}`;
+    day.querySelector(".min").textContent = day
+      .querySelector(".min")
+      .classList.contains("metric")
+      ? `${dailyForecastArrayMin[index]}`
+      : `${imperialForecastArrayMin[index]}`;
+    day.querySelector(".max").textContent = day
+      .querySelector(".max")
+      .classList.contains("metric")
+      ? `${dailyForecastArrayMax[index]}`
+      : `${imperialForecastArrayMax[index]}`;
   });
 
   const hourlyTempArray = data.hourly.temperature_2m.slice(0, 8);
   const hourlyTimeArray = data.hourly.time.slice(0, 8);
 
-  console.log(hourlyTempArray);
-  console.log(hourlyTimeArray);
+  const imperialTempArray = imperialData.hourly.temperature_2m.slice(0, 8);
 
   const hours = document.querySelectorAll(".hours");
 
   hours.forEach((hour, index) => {
     hour.querySelector(".time").textContent = `${hourlyTimeArray[index]}`;
-    hour.querySelector(".hourly-temp").textContent =
-      `${hourlyTempArray[index]} degrees`;
+    hour.querySelector(".hourly-temp").textContent = hour
+      .querySelector(".hourly-temp")
+      .classList.contains("metric")
+      ? `${hourlyTempArray[index]} degrees`
+      : `${imperialTempArray[index]} farenheit`;
   });
 }
 
@@ -94,3 +127,19 @@ async function searchLocation() {
 
   console.log(data.results[0].admin1);
 }
+// Function to switch between metric / imperial
+
+const unitSelectBtn = document.querySelector("#unitSelect");
+
+unitSelectBtn.addEventListener("change", () => {
+  if (unitSelectBtn.value === "metricBtn") {
+    document.querySelector("#feelsLikeTemp").classList.add("metric");
+    document.querySelector("#currentTemp").classList.add("metric");
+    document.querySelector("#currentWindSpeed").classList.add("metric");
+  } else {
+    document.querySelector("#feelsLikeTemp").classList.remove("metric");
+    document.querySelector("#currentTemp").classList.remove("metric");
+  }
+
+  getCurrentWeather(lat, lon);
+});
